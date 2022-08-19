fc1b279a69ce6d23b71eed3bec38106e79e4531f
ivelhafzerm <fahlevir696@gmail.com>
2022-08-19 14:57:34 +0800

    skills section

--- js.js
+++ js.js
@@ -8,11 +8,12 @@ function home() {
 }
 
 //? Container animate from here!!!
-tl.from(".intro h1, .intro span,.intro h3, .hero-desc, .button-hero,.clonex", {
+tl.from(".pg1-text-hero h1,.intro h1, .intro span,.intro h3, .hero-desc, .button-hero,.clonex", {
   opacity: 0,
   ease: "power2.easeOut",
   y: 50,
   stagger: 0.5,
+  duration: 1.4,
 })
   .from(".logo-svg svg", {
     opacity: 0,
